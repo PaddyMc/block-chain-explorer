59cf2ae59b1d008822befeda89c35f32105aa0ce
PaddyMc <paddymchale@hotmail.com>
2018-04-29 19:34:36 +0100

    pulling data from local node

--- dbUtils.js
+++ dbUtils.js
@@ -2,32 +2,43 @@ const cassandra = require('cassandra-driver');
 
 const cassandraClient = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'blockchainexplorer' });
 
-const queryGetAllBlocksFromDB = 'SELECT parentHash, number, time, witnessAddress, transactionsCount,transactions FROM block';
+const queryGetAllBlocksFromDB = 'SELECT parentHash, number, time, witnessAddress, transactionsCount, transactions FROM block';
 const queryGetTransactionsFromBlock = 'SELECT number, transactionsCount,transactions FROM block WHERE number = ?';
 const queryInsertBlock = 'INSERT INTO block (parentHash, number, time, witnessAddress, transactionsCount, transactions) VALUES (?, ?, ?, ?, ?, ?);';
 
-function getAllBlocks(){
-	cassandraClient.execute(queryGetAllBlocksFromDB)
-		.then(result => console.log(result));
+class CassandraDBUtils {
+	constructor(construction) {
+		console.log(construction);
+	}
+
+	getAllBlocks(){
+		cassandraClient.execute(queryGetAllBlocksFromDB)
+			.then(result => console.log(result));
+	}
+
+	getTransactionsFromBlockNumber(blockNum){
+		cassandraClient.execute(queryGetTransactionsFromBlock, [ blockNum ], { prepare: true })
+		.then(result => {
+			const row = result.first();
+			console.log(row);
+		});	
+	}
+
+	insertBlock(params){
+		//var transaction1 = { blocknum: 1, fromAddress: 'val2', toAddress: 'val3', amount: 100 };
+		//var transaction2 = { blocknum: 1, fromAddress: 'val1', toAddress: 'val0', amount: 100 };
+		//const params = ['0000000000000004FC3D510BC1661E4E5905A4C197B07FEC05DC8D4784F0A898', 11, 0, '27YkUVSuvCK3K84DbnFnxYUxozpi793PTqZ', 0, { "qwresd": transaction1, "sdagf":transaction2 }]
+		//const params = [parentHash, number, time, witnessAddress, transactionsCount, transactions];
+
+		//{ "qwresd": transaction1, "sdagf":transaction2 }
+
+		cassandraClient.execute(queryInsertBlock, params, { prepare: true })
+	  		.then(result => console.log('Row updated on the cluster'));
+	}
 }
 
-function getTransactionsFromBlockNumber(blockNum){
-	cassandraClient.execute(queryGetTransactionsFromBlock, [ blockNum ], { prepare: true })
-	.then(result => {
-		const row = result.first();
-		console.log(row);
-	});	
-}
+module.exports = CassandraDBUtils;
 
-function insertBlock(){
-	var transaction1 = { blocknum: 1, fromAddress: 'val2', toAddress: 'val3', amount: 100 };
-	var transaction2 = { blocknum: 1, fromAddress: 'val1', toAddress: 'val0', amount: 100 };
-	const params = ['0000000000000004FC3D510BC1661E4E5905A4C197B07FEC05DC8D4784F0A898', 10, 0, '27YkUVSuvCK3K84DbnFnxYUxozpi793PTqZ', 0, { "qwresd": transaction1, "sdagf":transaction2 }];
-
-	cassandraClient.execute(queryInsertBlock, params, { prepare: true })
-  		.then(result => console.log('Row updated on the cluster'));
-}
-
-getAllBlocks();
-insertBlock();
-getTransactionsFromBlockNumber(10);
+//getAllBlocks();
+//insertBlock();
+//getTransactionsFromBlockNumber(10);
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('babel-core/register');
 require('babel-polyfill');
 
 //Cassandra DBUtils
-require('./dbUtils.js');
+const CassandraDBUtils = require('./dbUtils.js');
+
+// add get tronix price => https://api.coinmarketcap.com/v1/ticker/tronix/
 
 const BlockChainData = require('./explorer.js');
 var elasticsearch = require('elasticsearch');
@@ -11,8 +13,31 @@ var client = new elasticsearch.Client({
   host: 'localhost:9200'
 });
 
-var blockChainData = new BlockChainData("ThisIsOurDataObject");
-blockChainData.getLatestBlock();
+var blockChainData = new BlockChainData("GetDataFromBC");
+var cassandraDBUtils = new CassandraDBUtils("PersistDataInDB");
+
+function putDataIntoDatabase(){
+    //grpc
+    var dataPromise = blockChainData.getLatestBlockFromLocalNode();
+    dataPromise.then(function(dataFromLocalNode){
+        console.log(dataFromLocalNode);
+        //cassandraDBUtils.getAllBlocks();
+    });
+
+    var transaction1 = { blocknum: 1, fromAddress: 'val2', toAddress: 'val3', amount: 100 };
+    var transaction2 = { blocknum: 1, fromAddress: 'val1', toAddress: 'val0', amount: 100 };
+
+    const params = ['0000000000000004FC3D510BC1661E4E5905A4C197B07FEC05DC8D4784F0A898', 11, 0, '27YkUVSuvCK3K84DbnFnxYUxozpi793PTqZ', 0, { "qwresd": transaction1, "sdagf":transaction2 }]
+    cassandraDBUtils.insertBlock(params);
+    //cassandraDBUtils.getAllBlocks();
+    //blockChainData.getBlockFromLocalNode(0);
+
+    //http
+    //blockChainData.getBlockByNum(0)
+    //blockChainData.getLatestBlock();
+}
+
+putDataIntoDatabase();
 
 function insertDataFromTronBCIntoElasticSearch(){
   var promiseWithTronData = blockChainData.getAllBlocksAsBulkRequest();
@@ -46,5 +71,5 @@ function insertDataFromTronBCIntoElasticSearch(){
     });
 }
 
-insertDataFromTronBCIntoElasticSearch();
+//insertDataFromTronBCIntoElasticSearch();
 

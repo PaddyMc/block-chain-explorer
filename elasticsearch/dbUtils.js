e7a9bd7e87c2999118b238dde4cb387a2624fd87
Cael Rowley <caelrowley@gmail.com>
2018-05-01 00:03:15 +0100

    Search witnesses on elastic search

--- elasticsearch/dbUtils.js
+++ elasticsearch/dbUtils.js
@@ -6,54 +6,74 @@ class ElasticSearchDBUtils {
 		console.log(construction);
 	}
 
-	insertBulk(jsonData){
-		var bulkRequest = _prepareBulkRequest(jsonData);
+	insertBlocks(jsonData){
+		let bulkData;
+		let bulkRequest = [];
 
-		var insertData = function(){
-			var busy = false;
-			var callback = function(error, response) {
-				if (error) {
-					console.log(error);
-				}
-				busy = false;
+		for(var i in jsonData["rows"]) {
+			var row = JSON.parse(jsonData["rows"][i]['[json]']);
+			bulkData = {
+				parentHash: row.parenthash,
+				number: row.number,
+				time: row.time,
+				witnessAddress: row.witnessaddress,
+				transactions: row.transactions,
+				transactionsCount: row.transactionscount
 			};
+			bulkRequest.push({index: {_index: 'blocks', _type: 'block', _id: row.number}});
+			bulkRequest.push(bulkData);
+		}
+		_insertBulk(bulkRequest)
+	}
 
-			if (!busy) {
-				busy = true;
-				client.bulk({
-					body: bulkRequest.slice(0, 1000)
-				}, callback);
-				bulkRequest = bulkRequest.slice(1000);
-			}
+	insertWitnesses(jsonData){
+		let bulkData;
+		let bulkRequest = [];
 
-			if (bulkRequest.length > 0) {
-				setTimeout(insertData, 10);
-			} else {
-				console.log('Inserted all blocks into elasticsearch.');
-			}
-		};
-      insertData();
+		for(var i in jsonData["rows"]) {
+			var row = JSON.parse(jsonData["rows"][i]['[json]']);
+			bulkData = {
+				address: row.address,
+				votecount: row.votecount,
+				pubkey: row.pubkey,
+				url: row.url,
+				totalmissed: row.totalmissed,
+				latestblocknum: row.latestblocknum,
+				latestslotnum: row.latestslotnum,
+				isjobs: row.isjobs
+			};
+			bulkRequest.push({index: {_index: 'witnesses', _type: 'witness', _id: row.address}});
+			bulkRequest.push(bulkData);
+		}
+		_insertBulk(bulkRequest)
 	}
 }
 
-function _prepareBulkRequest(jsonData){
-	let blockData;
-	let bulkRequest = [];
-
-	for(var k in jsonData["rows"]) {
-		var row = JSON.parse(jsonData["rows"][k]['[json]']);
-		blockData = {
-			parentHash: row.parenthash,
-			number: row.number,
-			time: row.time,
-			witnessAddress: row.witnessaddress,
-			transactions: row.transactions,
-			transactionsCount: row.transactionscount
+function _insertBulk(bulkRequest){
+	var insertData = function(){
+		var busy = false;
+		var callback = function(error, response) {
+			if (error) {
+				console.log(error);
+			}
+			busy = false;
 		};
-		bulkRequest.push({index: {_index: 'blocks', _type: 'block', _id: row.number}});
-		bulkRequest.push(blockData);
-	}
-	return bulkRequest;
+
+		if (!busy) {
+			busy = true;
+			client.bulk({
+				body: bulkRequest.slice(0, 1000)
+			}, callback);
+			bulkRequest = bulkRequest.slice(1000);
+		}
+
+		if (bulkRequest.length > 0) {
+			setTimeout(insertData, 10);
+		} else {
+			console.log('Inserted all blocks into elasticsearch.');
+		}
+	};
+  insertData();
 }
 
 module.exports = ElasticSearchDBUtils;

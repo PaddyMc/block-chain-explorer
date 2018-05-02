51a92c49a7eaf4cf4a6cb20cc00f0f6f4eef4043
PaddyMc <paddymchale@hotmail.com>
2018-05-02 21:15:33 +0100

    added witness page

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const ElasticSearchDBUtils = require('./elasticsearch/dbUtils.js');
 const BlockChainData = require('./blockchaindata/explorer.js');
 const BlockToDB = require("./datatransfer/blockToDB.js");
 const DBToElasticSearch = require("./datatransfer/dbToElasticsearch.js");
-const RestEndpoints = require("./datatransfer/RestEndpoints.js");
 
 //DBUtils
 var cassandraDBUtils = new CassandraDBUtils("Cassandra DBUtils created");
@@ -18,7 +17,6 @@ var blockChainData = new BlockChainData(GRPC_HOSTNAME_PORT);
 //DTO's
 var blocktoDB = new BlockToDB(blockChainData, cassandraDBUtils);
 var dbToElasticSearch = new DBToElasticSearch(cassandraDBUtils, elasticSearchDBUtils);
-var restEndpoints = new RestEndpoints(blockChainData);
 
 // Put data into DB
 //blocktoDB.putAllBlockDataIntoDB();      //   0-100
@@ -42,8 +40,8 @@ var restEndpoints = new RestEndpoints(blockChainData);
 
 // add get tronix price => https://api.coinmarketcap.com/v1/ticker/tronix/
 
-// let dataPromise = blockChainData.getAssetIssueList();
-//
+// let dataPromise = blockChainData.listWitnesses();
+
 // dataPromise.then(function(dataFromLocalNode){
 //     console.log(dataFromLocalNode);
 // });

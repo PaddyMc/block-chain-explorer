4ba295ff796c42d03e5834de802b39024df7ba5d
PaddyMc <paddymchale@hotmail.com>
2018-05-05 11:45:53 +0100

    cleaned up folders

--- docker/elastic/elastic.sh
+++
@@ -1,4 +0,0 @@
-echo "# CORS settings." >> /usr/share/elasticsearch/config/elasticsearch.yml
-echo "http.cors.enabled: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
-echo "http.cors.allow-origin: \"*\"" >> /usr/share/elasticsearch/config/elasticsearch.yml
-echo "http.cors.allow-methods : OPTIONS, HEAD, GET, POST" >> /usr/share/elasticsearch/config/elasticsearch.yml
--- docker/elastic/setup.sh
+++
@@ -1,5 +0,0 @@
-docker-compose -f docker-compose.yml up -d
-
-#docker exec elasticsearch sysctl -w vm.max_map_count=262144
-
-#docker exec elasticsearch ./elastic.sh

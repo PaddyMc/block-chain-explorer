b19620cecfeabf1d0ec0813b188ad65c5feaf198
PaddyMc <paddymchale@hotmail.com>
2018-05-21 22:32:53 +0100

    timer on service layer

--- index.js
+++ index.js
@@ -6,66 +6,34 @@ const BlockChainData = require('./blockchaindata/explorer.js');
 const BlockToDB = require("./datatransfer/blockToDB.js");
 const DBToElasticSearch = require("./datatransfer/dbToElasticsearch.js");
 const BlockToElastic = require("./datatransfer/blockToElastic.js");
+const TronExTimer = require("./tronexservice/tronExTimer.js");
 
+const geoLocationUrl = "https://ipapi.co/";
 
 //DBUtils
-const elasticSearchSetup = {host: 'localhost:9200'};
+const elasticSearchSetup = { host: 'localhost:9200' };
 const elasticSearchDBUtils = new ElasticSearchDBUtils(elasticSearchSetup);
 
 const cassandraSetup = { contactPoints: ['127.0.0.1'], keyspace: 'blockchainexplorer' };
 const cassandraDBUtils = new CassandraDBUtils(cassandraSetup, elasticSearchDBUtils);
 
 //BlockChainData
-const GRPC_HOSTNAME_PORT = {hostname:"127.0.0.1", port:"50051"};
+const GRPC_HOSTNAME_PORT = { hostname:"127.0.0.1", port:"50051" };
 const blockChainData = new BlockChainData(GRPC_HOSTNAME_PORT);
-
-const geoLocationUrl = "https://ipapi.co/";
 
 //DTO's
 const blocktoDB = new BlockToDB(blockChainData, cassandraDBUtils, geoLocationUrl);
 const dbToElasticSearch = new DBToElasticSearch(cassandraDBUtils, elasticSearchDBUtils);
 const blockToElastic = new BlockToElastic(blockChainData, elasticSearchDBUtils);
 
+//Timer
+const tronExTimer = new TronExTimer(blocktoDB, dbToElasticSearch, blockToElastic);
 
-function putAllDataIntoDB(){
-	blocktoDB.putAllWitnessesIntoDB();
-	blocktoDB.putAllNodesIntoDB();
-	blocktoDB.putAllAccountsIntoDB();
-	blocktoDB.putAllIssuedAssetsIntoDB();
-}
-
-function putAllDataIntoElastic(){
-	dbToElasticSearch.putAllWitnessDataIntoElasticSearch();
-	dbToElasticSearch.putAllAccountsDataIntoElasticSearch();
-	dbToElasticSearch.putAllNodeDataIntoElasticSearch();
-	dbToElasticSearch.putAllIssuedAssetsIntoElasticSearch();
-
-	dbToElasticsearch.putAllTransactionsDataIntoElastic();
-
-	blockToElastic.putDynamicPropertiesIntoElastic();
-	blockToElastic.putTotalTransactionIntoElastic();
-}
+tronExTimer.start();
 
 //  ToDo
 //	Get last 100 Blocks // 30 seconds
-//	From last 100 blocks update accounts
-
-// setInterval(function(){
-// 	putAllDataIntoDB();
-// }, 600000);
-
-// setInterval(function(){
-// 	blocktoDB.putAllBlockDataIntoDB();
-// }, 800000);
-
-// setInterval(function(){
-// 	putAllDataIntoElastic();
-// }, 1000000);
-
-// setInterval(function(){
-// 	dbToElasticSearch.putAllBlockDataIntoElasticSearch();
-// }, 1200000);
-
+//	Fix accounts
 
 // let dataPromise = blockChainData.listAccounts();
 // dataPromise.then(function(dataFromLocalNode){
---
+++ tronexservice/tronExTimer.js
@@ -0,0 +1,56 @@
+class TronExTimer {
+	constructor(blocktoDB, dbToElasticSearch, blockToElastic){
+		this.blocktoDB = blocktoDB;
+		this.dbToElasticSearch = dbToElasticSearch;
+		this.blockToElastic = blockToElastic;
+	}
+
+	start(){
+		setInterval(function(){
+			this.putAllDataIntoDB();
+		}, 600000);
+
+		setInterval(function(){
+			this.blocktoDB.putAllBlockDataIntoDB();
+		}, 800000);
+
+		setInterval(function(){
+			this.putAllDataIntoElastic();
+		}, 1000000);
+
+		setInterval(function(){
+			this.dbToElasticSearch.putAllBlockDataIntoElasticSearch();
+		}, 1200000);
+
+	}
+
+	putAllBlockDataIntoDB(){
+		this.blocktoDB.putAllBlockDataIntoDB();
+	}
+
+	putAllBlockDataIntoElasticSearch(){
+		this.dbToElasticSearch.putAllBlockDataIntoElasticSearch();
+	}
+
+	putAllDataIntoDB(){
+		this.blocktoDB.putAllWitnessesIntoDB();
+		this.blocktoDB.putAllNodesIntoDB();
+		this.blocktoDB.putAllAccountsIntoDB();
+		this.blocktoDB.putAllIssuedAssetsIntoDB();
+	}
+
+	putAllDataIntoElastic(){
+		this.dbToElasticSearch.putAllWitnessDataIntoElasticSearch();
+		this.dbToElasticSearch.putAllAccountsDataIntoElasticSearch();
+		this.dbToElasticSearch.putAllNodeDataIntoElasticSearch();
+		this.dbToElasticSearch.putAllIssuedAssetsIntoElasticSearch();
+
+		this.dbToElasticSearch.putAllTransactionsIntoElasticSearch();
+
+		this.blockToElastic.putDynamicPropertiesIntoElastic();
+		this.blockToElastic.putTotalTransactionIntoElastic();
+	}
+
+}
+
+module.exports = TronExTimer;

cf40361f9a885d5f18cb1a9ff2a41734010f074e
Cael Rowley <caelrowley@gmail.com>
2018-05-21 22:38:56 +0100

    Added query to read all transactions from cass to elastic

--- cassandra/dbUtils.js
+++ cassandra/dbUtils.js
@@ -22,13 +22,13 @@ const queryGetAllAssetIssue 			=		'SELECT JSON ownerAddress, name, totalsupply, 
 const queryInsertAssetIssue 			=		'INSERT INTO assetissues (ownerAddress, name, totalsupply, trxnum, num, starttime, endtime, decayratio, votescore, description, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
 
 // ACCOUNTS
-const queryGetAllAccounts 				=		'SELECT JSON uuid, accountname, type, address, balance, voteslist, assetmap, latestoprationtime, frozenlist, bandwidth, createtime, allowance, latestwithdrawtime, code FROM accounts LIMIT ' + QUERY_LIMIT;
 const queryGetFirstAccountsPartition 	=		'SELECT JSON uuid, accountname, type, address, balance, voteslist, assetmap, latestoprationtime, frozenlist, bandwidth, createtime, allowance, latestwithdrawtime, code FROM accounts LIMIT ' + QUERY_LIMIT;
 const queryGetAccountsPartition 		=		'SELECT JSON uuid, accountname, type, address, balance, voteslist, assetmap, latestoprationtime, frozenlist, bandwidth, createtime, allowance, latestwithdrawtime, code FROM accounts WHERE token(uuid) > token(?) LIMIT ' + QUERY_LIMIT;
 const queryInsertAccount 				=		'INSERT INTO accounts (uuid, accountname, type, address, balance, voteslist, assetmap, latestoprationtime, frozenlist, bandwidth, createtime, allowance, latestwithdrawtime, code) VALUES (uuid(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
 
 // TRANSACTIONS
-const queryGetAllTransactions 			= 		'SELECT JSON uuid, blocknum, transactionnum, fromaddress, toaddress, amount FROM transactions LIMIT ' + QUERY_LIMIT;
+const queryGetFirstTransactionsPartition 	=		'SELECT JSON uuid, blocknum, transactionnum, fromaddress, toaddress, amount FROM transactions LIMIT ' + QUERY_LIMIT;
+const queryGetTransactionsPartition 		=		'SELECT JSON uuid, blocknum, transactionnum, fromaddress, toaddress, amount FROM transactions WHERE token(uuid) > token(?) LIMIT ' + QUERY_LIMIT;
 const queryInsertTransaction 			=		'INSERT INTO transactions (uuid, blocknum, transactionnum, fromaddress, toaddress, amount) VALUES (uuid(), ?, ?, ?, ?, ?);';
 
 class CassandraDBUtils {
@@ -88,8 +88,39 @@ class CassandraDBUtils {
   	}
 
   	async getAllTransactions(){
+		let that = this;
+		var dataPromise = that.getAll(queryGetFirstTransactionsPartition);
+		dataPromise.then(function(dataFromLocalNode){
+			if(dataFromLocalNode["rows"][0]){
+				that.elasticSearchDBUtils.insertTransactions(dataFromLocalNode);
+			}
+			if(dataFromLocalNode["rows"][QUERY_LIMIT-1]){
+				var row = JSON.parse(dataFromLocalNode["rows"][QUERY_LIMIT-1]['[json]']);
+				let latestUuid = row.uuid;
+				that._getTransactionsPartition(latestUuid)
+			} else {
+				console.log("All transactions are read");
+			}
+		});
+
 		return this.getAll(queryGetAllTransactions);
   	}
+
+	_getTransactionsPartition(latestUuid){
+		let that = this;
+		let dataPromise = this.cassandraClient.execute(queryGetTransactionsPartition, [latestUuid], { prepare: true });
+		dataPromise.then(async function(dataFromLocalNode){
+			if(dataFromLocalNode["rows"][QUERY_LIMIT-1]){
+				let row = JSON.parse(dataFromLocalNode["rows"][QUERY_LIMIT-1]['[json]']);
+				let latestUuid = row.uuid;
+				that.elasticSearchDBUtils.insertTransactions(dataFromLocalNode);
+				that._getTransactionsPartition(latestUuid)
+			} else if(dataFromLocalNode["rows"][0]){
+				that.elasticSearchDBUtils.insertTransactions(dataFromLocalNode);
+				console.log("All transactions are read");
+			}
+		});
+	}
 
   	async getAllAccounts(){
 		let that = this;
